ddba8536bc77907ea538e3d19726b4af0afadf3a
Tiago Souza <tiagoice@hotmail.com>
2016-07-23 15:36:23 -0300

    Validador de extensao na classe Leitora

--- src/br/com/tiagods/model/Leitora.java
+++ src/br/com/tiagods/model/Leitora.java
@@ -10,15 +10,19 @@ import org.apache.pdfbox.text.PDFTextStripper;
 public class Leitora {
     public boolean verificarTexto(File arquivo, String texto){
             PDDocument pdfDocumento = null;
-            try{
-                    pdfDocumento = PDDocument.load(arquivo);
-                    PDFTextStripper stripper = new PDFTextStripper();
-                    StringBuilder sb = new StringBuilder();
-                    sb.append(stripper.getText(pdfDocumento));
+            try{    
+                    String extensao = arquivo.getName().substring(arquivo.getName().lastIndexOf(".")+1);
+                    if(extensao.equals("pdf")){
+                        pdfDocumento = PDDocument.load(arquivo);
+                        PDFTextStripper stripper = new PDFTextStripper();
+                        StringBuilder sb = new StringBuilder();
+                        sb.append(stripper.getText(pdfDocumento));
 
-                    if(sb.toString().toUpperCase().contains(texto.toUpperCase())){
-                        return true;
+                        if(sb.toString().toUpperCase().contains(texto.toUpperCase()))
+                            return true;
                     }
+                    else
+                        return false;
             }catch(IOException e){
                     e.printStackTrace();
             }finally{
